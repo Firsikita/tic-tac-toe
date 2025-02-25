51169a38c2448fef5f73be745ca062b29e0823ba
futig <everymusic8@gmail.com>
2025-02-25 16:25:29 +0500

    second

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const container = document.getElementById('fieldWrapper');
 startGame();
 addResetListener();
 
+// Инициализация
 function startGame() {
     isGameOver = false;
     movesCount = 0;
@@ -73,7 +74,103 @@ function aiMove() {
     }
 }
 
+function checkWinner() {
+    for (let row = 0; row < size; row++) {
+        if (
+            field[row][0] !== EMPTY &&
+            field[row].every((cell) => cell === field[row][0])
+        ) {
+            highlightCells(row, 0, row, size - 1); 
+            return field[row][0];
+        }
+    }
 
+    for (let col = 0; col < size; col++) {
+        let columnValues = [];
+        for (let row = 0; row < size; row++) {
+            columnValues.push(field[row][col]);
+        }
+
+        if (
+            columnValues[0] !== EMPTY &&
+            columnValues.every((val) => val === columnValues[0])
+        ) {
+            // Победный столбец
+            highlightCells(0, col, size - 1, col);
+            return columnValues[0];
+        }
+    }
+
+    let mainDiag = [];
+    for (let i = 0; i < size; i++) {
+        mainDiag.push(field[i][i]);
+    }
+    if (
+        mainDiag[0] !== EMPTY &&
+        mainDiag.every((val) => val === mainDiag[0])
+    ) {
+        highlightDiagonal(true);
+        return mainDiag[0];
+    }
+
+    let sideDiag = [];
+    for (let i = 0; i < size; i++) {
+        sideDiag.push(field[i][size - 1 - i]);
+    }
+    if (
+        sideDiag[0] !== EMPTY &&
+        sideDiag.every((val) => val === sideDiag[0])
+    ) {
+        highlightDiagonal(false);
+        return sideDiag[0];
+    }
+
+    return null;
+}
+
+function highlightCells(rowStart, colStart, rowEnd, colEnd) {
+    if (rowStart === rowEnd) {
+        for (let col = colStart; col <= colEnd; col++) {
+            renderSymbolInCell(field[rowStart][col], rowStart, col, 'red');
+        }
+    } else if (colStart === colEnd) {
+        for (let row = rowStart; row <= rowEnd; row++) {
+            renderSymbolInCell(field[row][colStart], row, colStart, 'red');
+        }
+    }
+}
+
+function highlightDiagonal(main) {
+    if (main) {
+        for (let i = 0; i < size; i++) {
+            renderSymbolInCell(field[i][i], i, i, 'red');
+        }
+    } else {
+        for (let i = 0; i < size; i++) {
+            renderSymbolInCell(field[i][size - 1 - i], i, size - 1 - i, 'red');
+        }
+    }
+}
+
+function endGame(winner) {
+    alert(`Победил ${winner}!`);
+    isGameOver = true;
+}
+
+function renderGrid(dimension) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < dimension; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = EMPTY;
+            cell.addEventListener('click', () => cellClickHandler(i, j));
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
 
 function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
